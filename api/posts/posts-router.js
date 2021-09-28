5abb896d7a2e99ef4ecfdcae8fc7f1f5d27df892
Klove Adams <kloveadams@gmail.com>
2021-09-28 18:26:57 -0400

    test #4 sad path for findById passes

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -8,6 +8,22 @@ router.get("/", async (req, res) => {
     res.status(200).json(posts);
   } catch (err) {
     res.status(500).json({
+      message: "The posts information could not be retrieved"
+    });
+  };
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+    const post = await Post.findById(id);
+    if (!post) {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist"
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
       message: err.message,
     });
   };
