96471c3afd9c1ad7cf7bd5f3cc4ac838f3522c36
Klove Adams <kloveadams@gmail.com>
2021-09-28 18:30:15 -0400

    test #3 happy path for findById passes

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -21,10 +21,12 @@ router.get("/:id", async (req, res) => {
       res.status(404).json({
         message: "The post with the specified ID does not exist"
       });
-    }
+    } else {
+      res.status(200).json(post);
+    };
   } catch (err) {
     res.status(500).json({
-      message: err.message,
+      message: "The post information could not be retrieved"
     });
   };
 });
