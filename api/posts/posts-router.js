31020a9d43f35f525058cb5c8804088915acd350
Klove Adams <kloveadams@gmail.com>
2021-09-28 18:58:04 -0400

    test #5&7 sad paths for post passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -31,4 +31,23 @@ router.get("/:id", async (req, res) => {
   };
 });
 
+router.post("/", async (req, res) => {
+  try {
+    const { title, contents } = req.body;
+    if (!title || !contents) {
+      res.status(400).json({
+        message: "Please provide title and contents for the post"
+      });
+    } else {
+      const newPost = await Post.insert({ title, contents });
+      console.log(newPost);
+      res.status(201).json(newPost);
+    };
+  } catch (err) {
+    res.status(500).json({
+      message: "There was an error while saving the post to the database"
+    });
+  };
+});
+
 module.exports = router;
