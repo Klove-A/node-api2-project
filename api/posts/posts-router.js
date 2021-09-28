e6ee629677448230101aefd8e5f2de60ab5af76d
Klove Adams <kloveadams@gmail.com>
2021-09-28 18:17:20 -0400

    test for get all posts passing

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,8 +1,16 @@
-// implement your posts router here
 const express = require("express")
 const Post = require("./posts-model");
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  };
+});
 
-
-module.exports = router
+module.exports = router;
