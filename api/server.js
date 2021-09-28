4faedf689aa81dd2accd7fba43d2a49d55fcbed4
Klove Adams <kloveadams@gmail.com>
2021-09-28 15:54:36 -0400

    posts-router.js connected

--- api/server.js
+++ api/server.js
@@ -3,5 +3,16 @@ const express = require("express");
 
 const server = express();
 // require your posts router and connect it here
+const postsRouter = require("./posts/posts-router")
+
+server.use(express.json());
+
+server.use("/api/posts", postsRouter);
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>inside server.js</h>
+  `);
+});
 
 module.exports = server;
