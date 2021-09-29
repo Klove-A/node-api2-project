e11a58d5c11c63c4ba7537266bc46768457d9688
Klove Adams <kloveadams@gmail.com>
2021-09-28 21:23:07 -0400

    Finished cleaning up for Gabe

--- api/server.js
+++ api/server.js
@@ -1,12 +1,8 @@
-// implement your server here
 const express = require("express");
-
 const server = express();
-// require your posts router and connect it here
 const postsRouter = require("./posts/posts-router")
 
 server.use(express.json());
-
 server.use("/api/posts", postsRouter);
 
 server.get('/', (req, res) => {
